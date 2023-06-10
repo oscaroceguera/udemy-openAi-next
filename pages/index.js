f5f5b1b9c43a5fe526a91dd6140e26a25e221ea0
Oscar E. Oceguera Bibriesca <oscar.oceguera@payclip.com>
2023-06-10 02:09:49 -0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+  console.log("🚀 ~ file: index.js:6 ~ Home ~ user:", user);
+  return (
+    <div>
+      <h1>this is the Homepage</h1>
+      <div>
+        {!!user ? (
+          <div>
+            <div>
+              <Image
+                src={user.picture}
+                alt={user.name}
+                width={50}
+                height={50}
+              />
+              <div>{user.email}</div>
+            </div>
+            <Link href="/api/auth/logout">Logout</Link>
+          </div>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
+      </div>
+    </div>
+  );
 }
---
+++ pages/post/new.js
@@ -0,0 +1,17 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+export default function NewPost(props) {
+  return (
+    <div>
+      <h1>This is the new post page</h1>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {
+      test: "this is a test",
+    },
+  };
+});
